eca3f156e9890a1a226f730a76a37f5ce703c3d8
Carlos <86106523+Csc32@users.noreply.github.com>
2023-01-26 19:03:16 -0400

    Delete folders

--- rust/data_types/target/debug/data_types.d
+++
@@ -1,1 +0,0 @@
-/home/carlos/Escritorio/projects/learning-rust/rust/data_types/target/debug/data_types: /home/carlos/Escritorio/projects/learning-rust/rust/data_types/src/main.rs
--- rust/data_types/target/debug/deps/data_types-e5f8674086529b95.d
+++
@@ -1,5 +0,0 @@
-/home/carlos/Escritorio/projects/learning-rust/rust/data_types/target/debug/deps/data_types-e5f8674086529b95.rmeta: src/main.rs
-
-/home/carlos/Escritorio/projects/learning-rust/rust/data_types/target/debug/deps/data_types-e5f8674086529b95.d: src/main.rs
-
-src/main.rs:
